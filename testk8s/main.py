1593339f018cc97fda1a2c6f851584ccb6eb55b9
Tim Meiwald <tim.meiwald@gmail.com>
2024-01-28 17:12:18 +0000

    Changes to deplyoment

--- testk8s/main.py
+++ testk8s/main.py
@@ -19,5 +19,5 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=82, reload=False,
+    uvicorn.run(app, host="0.0.0.0", port=80, reload=False,
                 workers=1)
